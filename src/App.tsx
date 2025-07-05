77a42c697f66a31af10798ca5c71cdddeee77887
DhruvBShetty <dhruvbshetty@gmail.com>
2025-07-05 12:59:04 +0300

    First initialization

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+interface Product {
+  name: string;
+  price: number;
+  category: string;
+  description: string;
+  rating: number;
+}
+
+function App() {
+
+  return (
+    <>
+     <p>Hello</p>
+    </>
+  )
+}
+
+export default App
